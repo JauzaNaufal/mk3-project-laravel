5075f19b94f9b3729999197160d40e93b043bcc6
JauzaNaufal <541231095@student.smktelkom-pwt.sch.id>
2024-10-16 14:14:15 +0700

    add index and login page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index'); // merujuk ke file welocome.blade.php di folder resources view
+});
+
+Route::get('/welcome', function () {
+    return view('welcome'); // merujuk ke file welocome.blade.php di folder resources view
+});
+
+Route::get('/login', function () {
+    return view('login'); // merujuk ke file welocome.blade.php di folder resources view
 });
